c3cda7b5df0bf86b88debf2d298f208d0f81a91c
maria-dierkes <mfd63@drexel.edu>
2023-05-25 05:35:18 -0400

    nav

--- Deploy/js/scripts-min.js
+++ Deploy/js/scripts-min.js
@@ -1,19 +1,21 @@
 let burgerBtn = document.querySelector("#burger-btn");
 let mainMenu = document.querySelector("#main-menu");
 
-burgerBtn.addEventListener("click", () => {
-    console.log(CanSeeMenu);
+let canSeeMenu = false;
 
-    if(!CanSeeMenu){
+burgerBtn.addEventListener("click", () => {
+    console.log(canSeeMenu);
+
+    if(!canSeeMenu){
         mainMenu.classList.add("show-menu");
-        // canSeeMenu = true;
+        canSeeMenu = true;
     }else{
         mainMenu.classList.remove("show-menu");
-        // canSeeMenu = false;
+        canSeeMenu=false;
     }
 })
 
 mainMenu.addEventListener("click", () => {
     mainMenu.classList.remove("show-menu");
-    // canSeeMenu = false;
+    canSeeMenu = false;
 })
